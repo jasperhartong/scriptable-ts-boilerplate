79c682a5a83e85fccd73a15bca3a6ec86c1ccc1f
Jasper Hartong <jasperhartong@gmail.com>
2020-10-31 22:14:37 +0100

    Correct rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,22 @@
 import typescript from '@rollup/plugin-typescript';
-console.warn("rollup congif")
-export default {
-    input: ['code/emittime-script-own.ts', 'code/emittime-script-following.ts'],
-    output: {
-        dir: 'output',
-        format: 'cjs',
-        strict: false,
+
+export default [
+    {
+        input: ['code/emittime-script-own.ts'],
+        output: {
+            dir: 'output',
+            format: 'cjs',
+            strict: false
+        },
+        plugins: [typescript()]
     },
-    plugins: [typescript()]
-};
+    {
+        input: ['code/emittime-script-following.ts'],
+        output: {
+            dir: 'output',
+            format: 'cjs',
+            strict: false
+        },
+        plugins: [typescript()]
+    }
+];
