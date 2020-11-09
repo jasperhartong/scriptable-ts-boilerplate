c11a53832dad4fe3d0c437f5abfa7c193324269d
Jasper Hartong <jasperhartong@gmail.com>
2020-11-09 10:40:29 +0100

    Update @types/scriptable-ios Remove no longer necessary ts-ignores

--- widgets/code/utils/color.ts
+++ widgets/code/utils/color.ts
@@ -1,5 +1,4 @@
 export const DynamicColor = ({ lightColor, darkColor }: { lightColor: Color, darkColor: Color }): Color =>
-    // @ts-ignore
     Color.dynamic(lightColor, darkColor);
 
 export const DefaultColor = () => DynamicColor({ lightColor: Color.white(), darkColor: Color.black() })
--- widgets/code/utils/request-utils.ts
+++ widgets/code/utils/request-utils.ts
@@ -1,6 +1,5 @@
 export const RequestWithTimeout = (url: string, timeoutSeconds = 5) => {
     const request = new Request(url)
-    // @ts-ignore
     request.timeoutInterval = timeoutSeconds
     return request
 }
